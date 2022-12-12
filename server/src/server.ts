9e70fc626e3fb96d5cdce3b9996818523b1d0fc2
Alisa <alisabox@mail.ru>
2022-12-12 11:23:10 +0900

    feat: add eslint to server

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,17 +8,18 @@ const webSocket = new WebSocketServer({ server });
 
 webSocket.on('connection', socket => {
   socket.on('message', (data, isBinary) => {
-    const message = isBinary ? data : data.toString();
+    const message = isBinary ? data : JSON.stringify(data);
     webSocket.clients.forEach(client => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(message);
       }
-    })
-  })
+    });
+  });
 });
 
 const PORT = 4300;
 
 server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server is running on port ${PORT}`);
 });
