46acf4e99d7db2618a7a5041b5fc8f5a323e91da
Alisa <alisabox@mail.ru>
2022-12-13 23:53:51 +0900

    fix: update package-lock files

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,7 @@ const webSocket = new WebSocketServer({ server });
 
 webSocket.on('connection', socket => {
   socket.on('message', (data, isBinary) => {
-    const message = isBinary ? data : JSON.stringify(data);
+    const message = isBinary ? data : String(data);
     webSocket.clients.forEach(client => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(message);
